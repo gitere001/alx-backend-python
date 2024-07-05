aa391d0bc9b092c31fda6fe48a346fba3fb6c545
James gitere <sasmuypwa2019.com>
2024-07-05 11:33:37 +0300

    added docstrig

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,4 +5,14 @@ Defines a function that adds two numbers
 
 
 def add(a: float, b: float) -> float:
+    """
+    Adds two numbers together and returns the result.
+
+    Parameters:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of the two numbers.
+    """
     return a + b
