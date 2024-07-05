d899156b3e155362f1852669fb091f89dd0951b6
James gitere <sasmuypwa2019.com>
2024-07-05 12:18:58 +0300

    implimented type annotation

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,27 +1,17 @@
 #!/usr/bin/env python3
-"""module safely_get_value"""
+'''Task 11: More involved type annotations
+'''
+from typing import Any, Mapping, Union, TypeVar
 
-from typing import Mapping, Any, Union, TypeVar
 
 T = TypeVar('T')
+Res = Union[Any, T]
+Def = Union[T, None]
 
 
-def safely_get_value(dct: Mapping[Any, Any], key: Any, default: Union[T, None]
-                     = None) -> Union[Any, T]:
-    """
-    Safely retrieves the value from a dictionary for a given key.
-
-    Args:
-        dct (Mapping[Any, Any]): The dictionary from which to retrieve the
-        value.
-        key (Any): The key for which to retrieve the value.
-        default (Union[T, None], optional): The default value to return if the
-        key is not found in the dictionary. Defaults to None.
-
-    Returns:
-        Union[Any, T]: The value associated with the key in the dictionary, or
-        the default value if the key is not found.
-    """
+def safely_get_value(dct: Mapping, key: Any, default: Def = None) -> Res:
+    '''Retrieves a value from a dict using a given key.
+    '''
     if key in dct:
         return dct[key]
     else:
--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python3
-"""module 102-type_checking"""
-from typing import Tuple, List, Any
+"""module for type checking"""
+from typing import List, Tuple
 
 
-def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> List[Any]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
-    Generates a new list by repeating each element in the input list `lst` `
-    factor` times.
+    Generates a new list by repeating each element in the input list `lst`
+    `factor` times.
 
     Parameters:
-        lst (Tuple[Any, ...]): The input list to be zoomed in.
+        lst (Tuple): The input list to be zoomed in.
         factor (int, optional): The number of times each element should be
         repeated. Defaults to 2.
 
     Returns:
-        List[Any]: The zoomed-in list.
+        List: The zoomed-in list with each element repeated `factor` times.
     """
-    zoomed_in: List[Any] = [
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
 
 
-array = (12, 72, 91)  # Should be a tuple
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
