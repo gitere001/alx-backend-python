7d72281b889a29c3ff2a2ec61bda07a937ea7cbb
James gitere <sasmuypwa2019.com>
2024-07-28 16:03:58 +0300

    implimented test memorize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 """test utils module"""
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from unittest.mock import patch, Mock
 
 
@@ -56,3 +56,33 @@ class TestGetJson(unittest.TestCase):
             # Assertions
             self.assertEqual(result, test_payload)
             mock_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """ Class for Testing Memoize """
+    def test_memoize(self):
+        """test memoize function"""
+        class TestClass:
+            """class for testing"""
+
+            def a_method(self):
+                """return 42"""
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_obj:
+            t = TestClass()
+            result1 = t.a_property
+            result2 = t.a_property
+            result2 = t.a_property
+            result2 = t.a_property
+            result2 = t.a_property
+            result2 = t.a_property
+            result2 = t.a_property
+            result2 = t.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            mock_obj.assert_called_once()
